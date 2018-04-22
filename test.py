51aac00d527c33da4efc6214c5fd69c663edb9e1
vista <vista.shao@gmail.com>
2018-04-22 11:14:57 +1000

    no msg

--- test.py
+++ test.py
@@ -2,76 +2,86 @@
 
 # http://sebsauvage.net/python/gui/
 # http://www.tkdocs.com/tutorial/tree.html
+# http://effbot.org/tkinterbook/
 
 import tkinter
 from tkinter import ttk
 
 
-class UI_Page_operation(tkinter.Tk):
+class UI_frm_node():
     def __init__(self, parent):
-        # tkinter.Tk.__init__(self, parent)
         self.parent = parent
         self.initialize()
 
     def initialize(self):
-        self.page = ttk.Frame(self.parent, padding=(5))
-        self.parent.add(self.page, text='Operation')
-        self.page.columnconfigure(0, weight=1)
-        self.page.rowconfigure(0, weight=1)
-
-        self.conf_add = ttk.Button(self.page, text='Add a node')
-        self.label = tkinter.Text(self.page, height=10)
-
-        self.conf_add.grid(column=1, row=3)
-        self.label.grid(column=0, row=0)
-        pass
-
-
-class UI_Page_configure(tkinter.Tk):
-    def __init__(self, parent):
-        # tkinter.Tk.__init__(self, parent)
-        self.parent = parent
-        self.initialize()
-
-    def initialize(self):
-        self.page = ttk.Frame(self.parent, padding=(5))
-        
-        self.parent.add(self.page, text='Configure')
-        self.page.columnconfigure(0, weight=1)
-        self.page.rowconfigure(0, weight=1)
-
-        self.conf_table = ttk.Treeview(self.page)
-        self.conf_table.insert('', 'end', 'widgets', text='Widget Tour')
-
-        self.conf_add = ttk.Button(self.page, text='Add a node')
-        self.conf_remove = ttk.Button(self.page, text='Remove a node')
-        self.conf_clean = ttk.Button(self.page, text='Clean all node')
-
-        self.conf_table.grid(column=0, row=0, columnspan=3)
-        self.conf_add.grid(column=1, row=3)
-        self.conf_remove.grid(column=2, row=3)
-        self.conf_clean.grid(column=3, row=3)
         pass
 
 
 class UI_LabTool(tkinter.Tk):
     def __init__(self, parent):
         tkinter.Tk.__init__(self, parent)
+        self.title("Lab Tool")
+        self.geometry('600x400')
         self.parent = parent
         self.initialize()
 
     def initialize(self):
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
+        self.grid()
 
-        self.nb = ttk.Notebook(self)
-        self.page_oper = UI_Page_operation(self.nb)
-        self.page_conf = UI_Page_configure(self.nb)
-        self.nb.pack(expand=1, fill="both")
+        self.frm_nodes = ttk.LabelFrame(self, text='Node')
+        self.frm_operate = ttk.LabelFrame(self, text='Task')
+        self.frm_status = ttk.LabelFrame(self, text='Log')
+
+        self.frm_nodes.grid(column=0, row=0, sticky=tkinter.NSEW)
+        self.frm_operate.grid(column=1, row=0, sticky=tkinter.NSEW)
+        self.frm_status.grid(
+            column=0, row=1, columnspan=2, sticky=tkinter.NW)
+
+        self.lbl_ip = ttk.Label(self.frm_nodes, text="IP")
+        self.enty_ip = ttk.Entry(self.frm_nodes)
+        self.lbl_ip.grid(row = 0, column = 0, sticky = tkinter.W)
+        self.enty_ip.grid(row = 1, column = 0)
+
+        self.lbl_base = ttk.Label(self.frm_nodes, text="Base Folder")
+        self.enty_base = ttk.Entry(self.frm_nodes)
+        self.lbl_base.grid(row = 0, column = 1, sticky = tkinter.W)
+        self.enty_base.grid(row = 1, column = 1)
+
+        self.lbl_user = ttk.Label(self.frm_nodes, text="User")
+        self.enty_user = ttk.Entry(self.frm_nodes)
+        self.lbl_user.grid(row = 0, column = 2, sticky = tkinter.W)
+        self.enty_user.grid(row = 1, column = 2)
+
+        self.lbl_pass = ttk.Label(self.frm_nodes, text="Password")
+        self.enty_pass = ttk.Entry(self.frm_nodes)
+        self.lbl_pass.grid(row = 0, column = 3, sticky = tkinter.W)
+        self.enty_pass.grid(row = 1, column = 3)
+
+        self.btn_add = ttk.Button(self.frm_nodes, text='Add node')
+        self.btn_remove = ttk.Button(self.frm_nodes, text='Remove node')
+        self.btn_edit = ttk.Button(self.frm_nodes, text='Edit node')
+        self.btn_add.grid(column=0, row=2, sticky=tkinter.NSEW)
+        self.btn_edit.grid(column=1, row=2, sticky=tkinter.NSEW)
+        self.btn_remove.grid(column=3, row=2, sticky=tkinter.NSEW)
+
+        self.tree_node = ttk.Treeview(self.frm_nodes, columns=('IP', 'Base Folder', 'User', 'Password'))
+        self.tree_node.heading('#0', text='IP')
+        self.tree_node.heading('#1', text='Base Folder')
+        self.tree_node.heading('#2', text='User')
+        self.tree_node.heading('#3', text='Password')
+        self.tree_node.column('#0', stretch=tkinter.YES)
+        self.tree_node.column('#1', stretch=tkinter.YES)
+        self.tree_node.column('#2', stretch=tkinter.YES)
+        self.tree_node.column('#3', stretch=tkinter.YES)
+        self.tree_node.grid(column=0, row=3, columnspan=4, sticky=tkinter.NSEW)
+
+        self.frm_nodes.columnconfigure('all', weight=1)
+
+        self.columnconfigure('all', weight=1)
+        self.rowconfigure('all', weight=1)
         pass
 
 
 if __name__ == '__main__':
     app = UI_LabTool(None)
-    app.title("Lab Tool")
     app.mainloop()
